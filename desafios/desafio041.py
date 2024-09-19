0e2893f50537f2df7231f73b78382d565fd50949
jugfarias <jufarias13@gmail.com>
2024-09-19 14:24:18 -0300

    categoria de natação de acordo com idade do atleta

---
+++ desafios/desafio041.py
@@ -0,0 +1,21 @@
+# A confederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
+# - Até 9 anos: MIRIM
+# - Até 14 anos: INFANTIL
+# - Até 19 anos: JUNIOR
+# - Até 25 anos: SÊNIOR
+# - Acima: MASTER
+
+print('=' * 10,'CONFEDERAÇÃO NACIONAL DE NATAÇÃO','=' * 10)
+
+idade = int(input('Qual sua idade? '))
+
+if idade <= 9:
+    print('Categoria: MIRIM')
+elif idade <= 14:
+    print('Categoria: INFANTIL')
+elif idade <= 19:
+    print('Categoria: JUNIOR')
+elif idade <= 25:
+    print('Categoria: SÊNIOR')
+else:
+    print('Categoria: MASTER')
