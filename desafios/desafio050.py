916457ed15403499e242ba934cda75d77c322cd5
jugfarias <jufarias13@gmail.com>
2024-09-20 21:29:22 -0300

    soma de números pares lidos

--- desafios/desafio050.py
+++ desafios/desafio050.py
@@ -1,1 +1,10 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
+soma = 0
+
+for i in range (1, 7):
+    num = int(input('Número {}: '.format(i)))
+    if num % 2 == 0:
+        soma += num
+
+print('A soma dos números pares passados é: {}'.format(soma))
