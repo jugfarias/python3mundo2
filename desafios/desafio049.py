fc71e8e64948f52a77d9c7836061ea01a16dcf3d
jugfarias <jufarias13@gmail.com>
2024-09-20 21:22:59 -0300

    tabuada com for

--- desafios/desafio049.py
+++ desafios/desafio049.py
@@ -1,1 +1,6 @@
 # Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+numero = int(input('Escolha um número de 1 a 10: '))
+
+for i in range (1, 11):
+    print('{} x {} = {}'.format(i, numero, i * numero))
