2bc1b70e64f664ee5a6192e217936dc454bbc75c
jugfarias <jufarias13@gmail.com>
2024-09-20 21:37:51 -0300

    verificação se é primo ou não

--- desafios/desafio052.py
+++ desafios/desafio052.py
@@ -1,1 +1,14 @@
 # Faça um program que leia um número inteiro e diga se ele é ou não um número primo.
+
+num = int(input('Número inteiro: '))
+eh_primo = True
+
+for i in range (2, num):
+    if num % i == 0:
+        eh_primo = False
+        exit()
+
+if eh_primo:
+    print('{} é um número primo.'.format(num))
+else:
+    print('{} não é um número primo.'.format(num))
