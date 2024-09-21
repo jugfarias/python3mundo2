19b341c84b635c8de82018a9bd6262bba72cc298
jugfarias <jufarias13@gmail.com>
2024-09-21 13:49:51 -0300

    for de trás pra frente para formar uma frase invertida e verificar se é um palíndromo

--- desafios/desafio053.py
+++ desafios/desafio053.py
@@ -5,3 +5,17 @@
 # A TORRE DA DERROTA
 # O LOBO AMA O BOLO
 # ANOTARAM A DATA DA MARATONA
+
+frase = input('Digite uma frase: ')
+frase = frase.replace(' ', '')
+
+frase_invertida = ''
+
+for i in range(len(frase) - 1, -1, -1):
+    frase_invertida += frase[i]
+
+if frase == frase_invertida:
+    print('É um palíndromo.')
+else:
+    print('Não é um palíndromo.')
+
