b3540d381336580018ade7ab8653d274c74d3391
jugfarias <jufarias13@gmail.com>
2024-09-21 13:58:22 -0300

    leitura de pesos e print de maior e menor

--- desafios/desafio055.py
+++ desafios/desafio055.py
@@ -1,1 +1,15 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+maior = 0
+menor = 300
+
+for i in range (0, 5):
+    peso = float(input('Digite seu peso em kg: '))
+
+    if peso > maior:
+        maior = peso
+    
+    if peso < menor:
+        menor = peso
+
+print('Maior peso: {}\nMenor peso: {}'.format(maior, menor))
