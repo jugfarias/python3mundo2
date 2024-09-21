d300630f7e9676dae996db3c705ae1e65bd16803
jugfarias <jufarias13@gmail.com>
2024-09-20 21:33:39 -0300

    progressão aritmética

--- desafios/desafio051.py
+++ desafios/desafio051.py
@@ -1,1 +1,8 @@
 # desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+a1 = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+
+for n in range (1, 11):
+    an = a1 + (n - 1) * r
+    print(an)
