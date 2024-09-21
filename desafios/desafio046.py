042523bce3d40f6042f4f7551c5d7ffd83299d4d
jugfarias <jufarias13@gmail.com>
2024-09-20 21:16:06 -0300

    contagem regressiva com sleep

--- desafios/desafio046.py
+++ desafios/desafio046.py
@@ -1,1 +1,7 @@
 # Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+
+for i in range (10, -1, -1):
+    print('{}{}{}!!'.format('\033[36m', i, '\033[m'))
+    sleep(1)
