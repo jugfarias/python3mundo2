22bb333d8940b6a4e5dd0c26987826c1afccfae0
jugfarias <jufarias13@gmail.com>
2024-09-21 13:54:34 -0300

    uso de date do datetime, verificação de maioridade de 7 pessoas

--- desafios/desafio054.py
+++ desafios/desafio054.py
@@ -1,1 +1,19 @@
 # Crie um programa que leia um ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
+from datetime import date
+
+ano_atual = date.today().year
+
+menores = 0
+maiores = 0
+
+for i in range (0, 7):
+    nasc = int(input('Digite seu ano de nascimento: '))
+    idade = ano_atual - nasc
+
+    if idade < 21:
+        menores += 1 
+    else:
+        maiores += 1
+
+print('{} pessoas são maiores de idade e {} são menores.'.format(maiores, menores))
