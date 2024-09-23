3a492f461f1e366440fb560724c9b068107f567c
jugfarias <jufarias13@gmail.com>
2024-09-23 13:04:46 -0300

    calculo de fatorial

--- desafios/desafio060.py
+++ desafios/desafio060.py
@@ -1,3 +1,16 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # Exemplo:
 # 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+num = int(input('Digite um número: '))
+fatorial = 1
+
+print('{}! = '.format(num), end = '')
+while num >= 1:
+    if num == 1:
+        print('{} = {}'.format(num, fatorial))
+    else:
+        print(num, end = ' x ')
+        
+    fatorial *= num
+    num -= 1
