2b8c4f4800538996a2ae47aea14a45756573a294
jugfarias <jufarias13@gmail.com>
2024-09-23 19:02:57 -0300

    melhorias no código de PA

--- desafios/desafio062.py
+++ desafios/desafio062.py
@@ -3,19 +3,23 @@
 a1 = int(input('Primeiro termo: '))
 r = int(input('Razão: '))
 resp = 1
-n_max = 10
+n_max = 0
+n = 1
+cont = 0
+resp = 10
 
 while resp != 0:
-    
-    n = 1
+    n_max += resp
+
     while n <= n_max:
         an = a1 + (n - 1) * r
         print(an, end = ' -> ')
 
         n += 1
 
-    resp = int(input('\nQuantos termos mais você quer ver? '))
-    
-    n_max += resp
+    print('PAUSA')
+
+    resp = int(input('Quantos termos mais você quer ver? '))
 
 print('ACABOU')
+print('Progressão finalizada com {} termos apresentados.'.format(n_max))
