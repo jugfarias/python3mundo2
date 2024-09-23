7e68feb4e50ddd7e6daf1265f72b9fa5d0d0cf3a
jugfarias <jufarias13@gmail.com>
2024-09-23 19:28:45 -0300

    print do maior e menor número da sequência passada

--- desafios/desafio065.py
+++ desafios/desafio065.py
@@ -1,12 +1,19 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
 continua = True
-soma = 0 
-cont = 0
+soma = cont = maior = menor = 0
 
 while continua:
-    soma += float(input('Digite um valor: '))
+    num = float(input('Digite um valor: '))
+    soma += num
     cont += 1
+
+    if cont == 1:
+        maior = menor = num
+    elif num > maior:
+        maior = num
+    elif num < menor:
+        menor = num
 
     resp = '0'
     while resp not in 'S N':
@@ -18,6 +25,8 @@ while continua:
         continua = False
     
 media = soma / cont
-print('A média dos valores digitados é: {}'.format(media))
+print('A média dos {} valores digitados é: {}'.format(cont, media))
+print('O maior valor foi: {}'.format(maior))
+print('O menor valor foi: {}'.format(menor))
 
 
