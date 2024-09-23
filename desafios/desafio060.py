ddbbe2aa009ee4c88717c08df7d8ff9ee610d224
jugfarias <jufarias13@gmail.com>
2024-09-23 18:44:14 -0300

    forma de resolver o fatorial com math.factorial e uso de for

--- desafios/desafio060.py
+++ desafios/desafio060.py
@@ -2,6 +2,8 @@
 # Exemplo:
 # 5! = 5 x 4 x 3 x 2 x 1 = 120
 
+from math import factorial
+
 num = int(input('Digite um número: '))
 fatorial = 1
 
@@ -14,3 +16,22 @@ while num >= 1:
         
     fatorial *= num
     num -= 1
+
+# Fazendo por método de math
+num = int(input('Digite um número: '))
+f = factorial(num)
+print('{}! = {}'.format(num, f))
+
+# Fazendo com for
+fatorial = 1
+
+num = int(input('Digite um número: '))
+
+print('{}! = '.format(num), end = '')
+for num in range (num, 0, -1):
+    if num == 1:
+        print('{} = {}'.format(num, fatorial))
+    else:
+        print(num, end = ' x ')
+        
+    fatorial *= num
