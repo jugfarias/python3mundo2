20db950da575af93247a68702fe42a1ea05dcad0
jugfarias <jufarias13@gmail.com>
2024-09-23 13:11:07 -0300

    progressão aritmética com while

--- desafios/desafio061.py
+++ desafios/desafio061.py
@@ -1,1 +1,14 @@
 # Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
+a1 = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+decimo = a1 + (10 - 1) * r
+n = 1
+
+while n <= 10:
+    an = a1 + (n - 1) * r
+    print(an, end = ' -> ')
+
+    n += 1
+
+print('ACABOU')
