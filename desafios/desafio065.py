67e9ec506e686e7f0ce3e70dc32ec66765710b63
jugfarias <jufarias13@gmail.com>
2024-09-23 12:57:03 -0300

    leitura e quantidade indefinida de valores e calculo da media, while dentro de while

--- desafios/desafio065.py
+++ desafios/desafio065.py
@@ -1,1 +1,23 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+continua = True
+soma = 0 
+cont = 0
+
+while continua:
+    soma += float(input('Digite um valor: '))
+    cont += 1
+
+    resp = '0'
+    while resp not in 'S N':
+        resp = input('Deseja continuar digitando números? [S/N] ').upper()
+    
+    if resp == 'S':
+        continua = True
+    else:
+        continua = False
+    
+media = soma / cont
+print('A média dos valores digitados é: {}'.format(media))
+
+
