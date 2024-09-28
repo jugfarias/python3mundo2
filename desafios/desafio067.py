fdbe68f00dfec4fb1f0641f7012d3e9b4dd79ec9
jugfarias <jufarias13@gmail.com>
2024-09-28 12:35:10 -0300

    tabuada com while e break

--- desafios/desafio067.py
+++ desafios/desafio067.py
@@ -1,1 +1,12 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+    num = int(input('Digite um número inteiro: '))
+
+    if num < 0:
+        break
+
+    i = 1
+    while i <= 10:
+        print(f'{i} x {num} = {i * num}')
+        i += 1
